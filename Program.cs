4f7f4150e4926eeeac4b154793fe9a1c6c79a717
wxhami <d18102002@mail.ru>
2022-12-06 15:22:27 +0300

    ex 1 done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿//ex 1
+
+List<int> list = new List<int>(){2,3,5,1,593,23,4,563,2,4,454,7,74,2,1,231,4,2,42,5,46,7};
+IEnumerable<int> result = list.Where(x => x > 2);
+var count = list.Count();
+
+var newList = list.Distinct();
+Console.Write(newList);
+
+//ex 2
+
